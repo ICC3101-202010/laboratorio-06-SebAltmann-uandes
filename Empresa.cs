1c31c065f59640588b97b656822deb1761bfabd4
Sebastián Altmann <saaltmann@miuandes.cl>
2020-05-19 01:34:30 -0400

    Finished: Parte 1

--- Empresa.cs
+++ Empresa.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Runtime.Serialization;
+
 namespace Laboratorio06
 {
+    [Serializable]
     public class Empresa
     {
-        public Empresa()
+        private string Nombre;
+        private string RUT;
+
+        public Empresa(String nombre, String rut)
         {
+            this.Nombre = nombre;
+            this.RUT = rut;
+        }
+
+        public string GetName()
+        {
+            return Nombre;
+        }
+
+        public string GetRUT()
+        {
+            return RUT;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+// using System.Exception;
 
 namespace Laboratorio06
 {
@@ -10,7 +11,74 @@ namespace Laboratorio06
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Empresa> Empresas = new List<Empresa>();
+
+            IFormatter formatter = new BinaryFormatter();
+
+            while (true)
+            {
+                String response = "X";
+                while(response != "S" && response != "s" && response != "N" && response != "n")
+                {
+                    Console.Write("Desea cargar los datos de una empresa?  {S/N}");
+                    response = Console.ReadLine();
+                    if (response != "S" && response != "s" && response != "N" && response != "n")
+                    {
+                        Console.WriteLine("Esa respuesta no es válida.");
+                    }
+                }
+
+                if (response.ToUpper() == "S")
+                {
+                    try
+                    {
+                        Stream stream = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                        Empresa empresa = (Empresa)formatter.Deserialize(stream);
+                        Empresas.Add(empresa);
+
+                        stream.Close();
+                    }
+                    catch(FileNotFoundException FNFE)
+                    {
+                        Console.WriteLine("No se pudo encontrar el archivo 'empresas.bin'.");
+                        Console.WriteLine(FNFE.Message);
+
+                        String new_nombre_e;
+                        String new_rut_e;
+
+                        Console.Write("Nombre de la empresa nueva: ");
+                        new_nombre_e = Console.ReadLine();
+
+                        Console.Write("RUT de la empresa nueva: (XX.XXX.XXX-X)  ");
+                        new_rut_e = Console.ReadLine();
+
+                        Empresa new_empresa = new Empresa(new_nombre_e, new_rut_e);
+
+                        Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                        formatter.Serialize(stream, new_empresa);
+                        stream.Close();
+                    }
+                }
+                else if (response.ToUpper() == "N")
+                {
+                    String new_nombre_e;
+                    String new_rut_e;
+
+                    Console.Write("Nombre de la empresa nueva: ");
+                    new_nombre_e = Console.ReadLine();
+
+                    Console.Write("RUT de la empresa nueva: (XX.XXX.XXX-X)  ");
+                    new_rut_e = Console.ReadLine();
+
+                    Empresa new_empresa = new Empresa(new_nombre_e, new_rut_e);
+
+                    Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                    formatter.Serialize(stream, new_empresa);
+                    stream.Close();
+                }
+
+                return;
+            }
         }
     }
 }
